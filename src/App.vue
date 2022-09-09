12350d1ac73a65204c4d1a04b0673ca6f50d309c
Paulo Correia <programmerpaulo@gmail.com>
2022-09-09 09:39:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div>
+  <ExemploTest />
+  
+</div>
+
+ </template>
+
+<script>
+import ExemploTest from "./components/ExemploTest.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ExemploTest
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ExemploTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Primeiro Componente</h1>
+    <q-input v-if="banana" class="q-gutter-md" style="max-width: 300px" v-model="text" label="Standard" />
+    <q-btn color="primary" label="Salvar" @click="salvar()"/>
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text:'',
+      lista:[],
+      numero:0,
+      banana:true
+    }
+  },
+  methods:{
+   salvar() {
+      alert(this.text)
+      this.banana = !this.banana
+
+     
+   },
+   outraFuncoa(){
+
+   }
+  }
+}
+</script>
+
+<style scoped>
+
+  h1 {
+    text-align: center;
+  }
+
+</style>
